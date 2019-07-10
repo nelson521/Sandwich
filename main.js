ddeae7cc555ecd825d9b427030c621827a0d3134
Stevenson Nelson <sonson521@gmail.com>
2019-07-09 23:58:38 -0400

    added html and vars and click function and alerts.

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+var pbj = 0;
+var gCheese = 0;
+var rBeef = 0;
+
+// any time we click the sandwich it will print at the h3 id.
+
+$('.pButter').on('click', function() {
+  // when this is clicked i want to write somwting on the h3
+  alert('Peanut butter jelly time');
+  pbj++
+  alert('You just had ' + pbj + ' peanut butter and jelly sandwichs.');
+});
+
+
+
+
+
+
+});
